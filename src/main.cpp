6d9dbeb5d012f093e054cb646983dbcc677ddb1b
averdura078 <averdura078@ed.amdsb.ca>
2026-03-06 14:51:24 -0500

    mar 6

--- src/main.cpp
+++ src/main.cpp
@@ -23,28 +23,63 @@ vex::motor leftTop = vex::motor(vex::PORT6, vex::gearSetting::ratio18_1, false);
 motor_group RightDrive = motor_group(rightFront, rightRear, rightTop);
 motor_group LeftDrive = motor_group(leftFront, leftRear, leftTop);
 
-vex::drivetrain Drivetrain = vex::drivetrain(RightDrive, LeftDrive, 3.25, 3.25, 14.5, distanceUnits::in, (0.75));
+inertial Inertial = inertial(PORT5);
+
+vex::smartdrive Drivetrain = vex::smartdrive(RightDrive, LeftDrive, Inertial, 3.25, 3.25, 14.5, distanceUnits::in, (0.75));
 // left, right, diameter of wheel, diameter of wheel, distance between wheels, distance units, gear ratio
 
-double p = 0.2;
-double target = 0;
-double error = 0;
-double currentDistance = 0;
+double kP = 0.5; // proportional gain constant (tune this value for your robot). Bigger = faster gain, Smaller = slower gain. If the robot is oscillating around the target distance, try reducing this value. If the robot is not reaching the target distance, try increasing this value.
+
+double error = 0;          // initialize error to 0
+double targetHeading = 0;  // initialize target heading to 0
+double currentHeading = 0; // initialize current heading to 0
+double motorPower = 0;     // initialize motor power to 0
+
+void turnCW();
+void turnCCW();
 
 int main()
-{    error = target - currentDistance;
+{
+    wait(5000, msec); //temporary
 
+    Inertial.calibrate(); // calibrate the inertial sensor
+    while (Inertial.isCalibrating())
+    {
+        wait(100, msec);
+    }
 
+    targetHeading = 90;                                           // set target heading to 90 degrees
+    currentHeading = Drivetrain.heading(vex::rotationUnits::deg); // measure current heading
 
+    error = targetHeading - currentHeading; // calculate error as the difference between target heading and current heading
+    motorPower = kP * error;                // calculate motor power using proportional control formula
 
+    if (motorPower > 90) // limit motor power to not burn out the motors
+        motorPower = 90;
+    if (motorPower < -90)
+        motorPower = 90;
 
+    //turnCW();
+Drivetrain.setHeading(targetHeading, rotationUnits::deg); // set the target heading for the drivetrain
 
-
-
-    
-            while (1)
+    while (1)
     {
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
+}
+
+void turnCW()
+{
+    LeftDrive.setVelocity(motorPower, percentUnits::pct);
+    RightDrive.setVelocity(-motorPower, percentUnits::pct);
+    LeftDrive.spin(forward);
+    RightDrive.spin(forward);
+}
+
+void turnCCW()
+{
+    LeftDrive.setVelocity(-motorPower, percentUnits::pct);
+    RightDrive.setVelocity(motorPower, percentUnits::pct);
+   
 }
