2dc3ef64eb685df079b2c1f6daa10366fef3ee0c
averdura078 <averdura078@ed.amdsb.ca>
2026-03-10 14:48:42 -0400

    mar 10

--- src/main.cpp
+++ src/main.cpp
@@ -29,55 +29,64 @@ inertial Inertial = inertial(PORT5);
 vex::smartdrive Drivetrain = vex::smartdrive(LeftDrive, RightDrive, Inertial, 3.25, 3.25, 14.5, distanceUnits::in, (0.75));
 // left, right, diameter of wheel, diameter of wheel, distance between wheels, distance units, gear ratio
 
-double kP = 0.5; // proportional gain constant (tune this value for your robot). Bigger = faster gain, Smaller = slower gain. If the robot is oscillating around the target distance, try reducing this value. If the robot is not reaching the target distance, try increasing this value.
+double kP = 0.3; // proportional gain constant (tune this value for your robot). Bigger = faster gain, Smaller = slower gain. If the robot is oscillating around the target, try reducing this value. If the robot is not reaching the target, try increasing this value.
 
+//turn these local later
 double error = 0;          // initialize error to 0
 double targetHeading = 0;  // initialize target heading to 0
 double currentHeading = 0; // initialize current heading to 0
 double motorPower = 0;     // initialize motor power to 0
 
+double turnWithP(double targetHeading); //define function before main so it can be called in main
+
 int main()
 {
-    wait(5000, msec); // temporary
-    
-    targetHeading = 90;
-
-    Inertial.calibrate(); // calibrate the inertial sensor
+    Inertial.calibrate(); // calibrate the inertial sensor before using it
     while (Inertial.isCalibrating())
     {
         wait(100, msec);
     }
 
+    turnWithP(90); // turn to 90 degrees using proportional control
+
     while (1)
-    {                                           // set target heading to 90 degrees
-        currentHeading = Inertial.heading(deg); // measure current heading
-
-        error = targetHeading - currentHeading; // calculate error as the difference between target heading and current heading
-        motorPower = kP * error;                // calculate motor power using proportional control formula
-
-        // clamp
-        if (motorPower > 100)
-            motorPower = 100;
-        if (motorPower < -100)
-            motorPower = -100;
-
-        // stop when close
-        if (fabs(error) < 1)
-        {
-            LeftDrive.stop();
-            RightDrive.stop();
-        }
-
-        // //take shortest path
-        // if (error > 180)
-        //     error -= 360;
-        // if (error < -180)
-        //     error += 360;
-
-        LeftDrive.spin(forward, motorPower, pct);
-        RightDrive.spin(forward, -motorPower, pct);
-
+    {
         // Allow other tasks to run
         this_thread::sleep_for(10);
     }
+}
+
+double turnWithP(double targetHeading)
+{
+    wait(5000, msec); // temporary
+
+    currentHeading = Inertial.heading(deg); // measure current heading
+
+    error = targetHeading - currentHeading; // calculate error as the difference between target heading and current heading
+
+    // take shortest path
+    if (error > 180)
+        error -= 360;
+    if (error < -180)
+        error += 360;
+
+    motorPower = kP * error; // calculate motor power using proportional control formula
+
+    // clamp to prevent motor burnout
+    if (motorPower > 100)
+        motorPower = 100;
+    if (motorPower < -100)
+        motorPower = -100;
+
+    LeftDrive.spin(forward, motorPower, pct);
+    RightDrive.spin(forward, -motorPower, pct);
+
+    // stop when close
+    if (fabs(error) < 1)
+    {
+        LeftDrive.stop();
+        RightDrive.stop();
+    }
+
+    return 1;
 }
