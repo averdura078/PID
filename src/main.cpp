71357b233748a66f7680a2cd917be3e7d853e1db
averdura078 <averdura078@ed.amdsb.ca>
2026-03-24 14:50:20 -0400

    working turnpid in function, not working drivepid

--- src/main.cpp
+++ src/main.cpp
@@ -32,13 +32,13 @@ inertial Inertial = inertial(PORT5);
 vex::smartdrive Drivetrain = vex::smartdrive(RightDrive, LeftDrive, Inertial, 3.25, 3.25, 14.5, distanceUnits::in, (0.75));
 // left, right, diameter of wheel, diameter of wheel, distance between wheels, distance units, gear ratio
 
-double kP = 0.37; // proportional gain constant (tune this value for your robot). Bigger kP = faster gain, Smaller kP= slower gain. If the robot is oscillating around the target distance, try reducing this value. If the robot is not reaching the target distance, try increasing this value.
+double tP = 0.4; // proportional gain constant (tune this value for your robot). Bigger kP = faster gain, Smaller kP= slower gain. If the robot is oscillating around the target distance, try reducing this value. If the robot is not reaching the target distance, try increasing this value.
 /*NOTES ABOUT PROPORTIONAL CONTROL
 - proportional = kP * error (this means the product of the proportional gain constant and the error)
 - the proportional term reacts proportionally to the error, meaning the bigger the error, the stronger the motor power.
 - proportional control is the main component of a PID controller, and is responsible for getting the robot to the target distance
 */
-double kD = 0.1; // derivative gain constant (tune this value for your robot). Bigger kD = stronger braking effect. Smaller kD = weaker breaking effect.
+double tD = 0.1; // derivative gain constant (tune this value for your robot). Bigger kD = stronger braking effect. Smaller kD = weaker breaking effect.
 /*NOTES ABOUT DERIVATIVE CONTROL
 - derivative = d(error)/dt (this means the derivative of the error over the derivative of the time, or the change in error over the change in time)
 - this is simplified to derivative = (error - previousError) / ∆t (this means the change in error over the change in time. Change in time is how big the delay in your loop is. If your loop runs every 10 milliseconds, then ∆t is 0.01 seconds.)
@@ -48,7 +48,7 @@ double kD = 0.1; // derivative gain constant (tune this value for your robot). B
 - derivative is positive when error is increasing, meaning the robot is pushed away from the target.
 - derivative prevents overshooting and oscillation around the target.
 */
-double kI = 0.09; // integral gain constant (tune this value for your robot).
+double tI = 0.1; // integral gain constant (tune this value for your robot).
 /*NOTES ABOUT INTEGRAL CONTROL
 - integral is the accumulation of error over time, meaning it is the sum of all past errors.
 - integral += error * ∆t (this means integral equals current value of integral plus the product of the current error multiplied by how much time passed in seconds).
@@ -57,6 +57,10 @@ double kI = 0.09; // integral gain constant (tune this value for your robot).
 - put simply, the integral “remembers” that the robot has been behind the target for a while, gradually adding more power until the error disappears.
 */
 
+double dP = 0.5;
+double dI = 0.3;
+double dD = 0.01;
+
 double motorPower = 0; // initialize motor power to 0
 
 double error = 0;         // initialize error to 0
@@ -64,20 +68,74 @@ double previousError = 0; // initialize previous error to 0
 double derivative = 0;    // initialize derivative to 0
 double integral = 0;      // initialize integral to 0
 
-double targetRotation = 0;  // initialize target rotation to 0
+double targetRotation = 90; // initialize target rotation to 0
 double currentRotation = 0; // initialize current rotation to 0
 
-int main()
+double targetDistance = 0;  // initialize target distance to 0
+double currentDistance = 0; // initialize current distance to 0
+
+void driveWithPID(double target)
 {
-    Inertial.calibrate(); // calibrate the inertial sensor
-    while (Inertial.isCalibrating())
+    targetDistance = target;
+    while (true)
     {
-        wait(100, msec);
+        double leftDegrees = leftFront.position(rotationUnits::deg);
+        double rightDegrees = rightFront.position(rotationUnits::deg);
+        double averageDegrees = (leftDegrees + rightDegrees) / 2.0;
+
+        currentDistance = (M_PI * 3.25) * averageDegrees / 360.0;
+
+        // Use this for troubleshooting. May use brain screen instead and check other values too.
+        Controller.Screen.setCursor(1, 1);
+        Controller.Screen.print("Current Rotation: %f", currentRotation);
+        Controller.Screen.setCursor(2, 1);
+        Controller.Screen.print("Error: %f", error);
+
+        // P
+        error = targetDistance - currentDistance; // calculate error as the difference between target distance and current distance
+
+        // D
+        derivative = error - previousError; // compute derivative as the change in error since the last loop iteration.
+        previousError = error;              // save the current error for the next loop iteration
+
+        // I
+        integral += error * (loopDelay / 1000.0); // compute integral as the accumulation of error as time passes (in seconds)
+        // clamp integral term to prevent integral windup (when the integral term accumulates too much error and causes the robot to overshoot the target)
+        //  if (integral > 200)
+        //      integral = 200;
+        //  if (integral < -200)
+        //      integral = -200;
+
+        // calculate motor power using PID control formula
+        motorPower = dP * error + dI * integral + dD * derivative;
+
+        // clamp to prevent motor burnout (don't let motors spin at more than 90% power)
+        if (motorPower > 90)
+            motorPower = 90;
+        if (motorPower < -90)
+            motorPower = -90;
+
+        // make the robot spin
+        LeftDrive.spin(forward, motorPower, percentUnits::pct);
+        RightDrive.spin(forward, motorPower, percentUnits::pct);
+
+        // uncomment the following lines if necessary to further prevent oscillation around the target
+        // if (fabs(error) <= 0.5) //stop if the error is less than or equal to 0.5 degrees
+        // {
+        //     LeftDrive.stop();
+        //     RightDrive.stop();
+        //     break;
+        // }
+
+        // allow other tasks to run
+        this_thread::sleep_for(loopDelay);
     }
+}
 
-    targetRotation = 90; // set target heading to 90 degrees
-
-    while (1)
+void turnWithPID(double target)
+{
+    targetRotation = target;
+    while (true)
     {
         // Use this for troubleshooting. May use brain screen instead and check other values too.
         Controller.Screen.setCursor(1, 1);
@@ -96,14 +154,14 @@ int main()
 
         // I
         integral += error * (loopDelay / 1000.0); // compute integral as the accumulation of error as time passes (in seconds)
-        //clamp integral term to prevent integral windup (when the integral term accumulates too much error and causes the robot to overshoot the target)
-        // if (integral > 200)
-        //     integral = 200;
-        // if (integral < -200)
-        //     integral = -200;
+        // clamp integral term to prevent integral windup (when the integral term accumulates too much error and causes the robot to overshoot the target)
+        //  if (integral > 200)
+        //      integral = 200;
+        //  if (integral < -200)
+        //      integral = -200;
 
         // calculate motor power using PID control formula
-        motorPower = kP * error + kI * integral + kD * derivative;
+        motorPower = tP * error + tI * integral + tD * derivative;
 
         // clamp to prevent motor burnout (don't let motors spin at more than 90% power)
         if (motorPower > 90)
@@ -120,9 +178,25 @@ int main()
         // {
         //     LeftDrive.stop();
         //     RightDrive.stop();
+        //     break;
         // }
 
         // allow other tasks to run
         this_thread::sleep_for(loopDelay);
+    }
+}
+
+int main()
+{
+    Inertial.calibrate(); // calibrate the inertial sensor
+    while (Inertial.isCalibrating())
+    {
+        wait(100, msec);
+    }
+
+    while (1)
+    {
+        driveWithPID(18); // drive forward 1 meter (39 inches)
+        // break;
     }
 }
